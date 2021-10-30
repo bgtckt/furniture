3b485b4b2ef67e83474dbd812f5b3551c0f6865e
Алексей Анпилов <bgtckt@gmail.com>
2021-10-30 12:21:12 +0300

    Добавил скрипты

---
+++ source/js/main_nav_mob.js
@@ -0,0 +1,13 @@
+//Открытие и закрытие главного меню в мобильной версии по нажатию кнопки
+
+const toggleButton = document.querySelector('.categories__button--burger');
+const mainNav = document.querySelector('.main-nav');
+const windowWidth = window.visualViewport.width;
+const tabletWidth = 768;
+const mobileWidth = 320;
+
+if (windowWidth <= mobileWidth) {
+  toggleButton.addEventListener('click', () => {
+    mainNav.classList.toggle('main-nav--closed');
+  });
+};
---
+++ source/js/slider.js
@@ -0,0 +1,23 @@
+//Реализация слайдера для переключения между элементами разделов
+
+function itemToggle(items, toggles, itemsClassName) {
+  for (let i = 0; i < items.length; i++) {
+    toggles[i].addEventListener('click', () => {
+      for (let j = 0; j < toggles.length; j++) {
+        items[j].classList.remove(itemsClassName + '--current');
+        toggles[j].classList.remove('toggles__button--current');
+      }
+      items[i].classList.add(itemsClassName + '--current');
+      toggles[i].classList.add('toggles__button--current');
+    });
+  };
+};
+
+const reviews = document.querySelectorAll('.reviews__block');
+const reviewsSlider = document.querySelector('.reviews__toggles');
+const reviewsButtons = reviewsSlider.querySelectorAll('.toggles__button');
+const reviewsClassName = 'reviews__block';
+
+//переключение между элементами раздела reviews
+
+itemToggle(reviews, reviewsButtons, reviewsClassName);
